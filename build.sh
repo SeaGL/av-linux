16d873651f9375f9aa13ac7263f670f12d95fd80
AJ Jordan <alex@strugee.net>
2024-09-25 17:40:58 -0400

    Configure Flathub

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+flatpak remote-add --system flathub --from flathub.flatpakrepo
+
 # Streaming packages
 rpm-ostree install obs-studio mpv yt-dlp
 # Presentation machine packages
