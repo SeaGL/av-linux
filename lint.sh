9c298de9c491e801d6bd5180e929b902b340a2b9
AJ Jordan <alex@strugee.net>
2024-10-10 02:22:13 -0400

    Check for `set -euo pipefail`

--- lint.sh
+++ lint.sh
@@ -8,3 +8,8 @@ trap 'echo Failed!' ERR
 echo 'Checking that all `flatpak` invocations pass `--system`.'
 # We use () to make this a subshell to avoid ! not triggering a `set -e` bailout; see bash(1)'s documentation on this flag for more
 ( ! grep -n flatpak *.sh | grep -v -- --system )
+
+for i in bin/* sbin/*; do
+	echo 'Checking that `'"$i"'` contains `set -euo pipefail`.'
+	grep -q 'set -euo pipefail' $i
+done
