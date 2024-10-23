a9837c0c5758be7d92693bb3f1028ee207d07006
AJ Jordan <alex@strugee.net>
2024-10-23 03:15:40 -0400

    Insist that Flatpak installs use --noninteractive

--- lint.sh
+++ lint.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 trap 'echo Failed!' ERR
 
 # See 8f01ed1063e61407dc71f01863b0256f5082fd94 for rationale
-echo 'Checking that all `flatpak` invocations pass `--system`.'
+echo 'Checking that all `flatpak` invocations pass `--noninteractive --system`.'
 # We use () to make this a subshell to avoid ! not triggering a `set -e` bailout; see bash(1)'s documentation on this flag for more
-( ! grep -n flatpak *.sh | grep -ve systemctl -e /var/lib/flatpak | grep -v -- --system )
+( ! grep -n flatpak *.sh | grep -ve systemctl -e /var/lib/flatpak -e 'remote-add.*--system' | grep -v -- '--noninteractive --system')
 
 for i in bin/* sbin/*; do
 	echo 'Checking that `'"$i"'` contains `set -euo pipefail`.'
