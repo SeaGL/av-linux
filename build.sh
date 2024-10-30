73eebdddde218fa144c2064bcb5b056f4a159973
AJ Jordan <alex@strugee.net>
2024-10-29 23:31:58 -0400

    Install GEdit

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ RELEASE="$(rpm -E %fedora)"
 flatpak remote-add --system flathub --from /tmp/flathub.flatpakrepo
 
 # General system packages
-rpm-ostree install jq age seahorse
+rpm-ostree install jq age seahorse gedit
 # Streaming machine packages
 rpm-ostree install mpv yt-dlp zenity
 flatpak install --noninteractive --system com.obsproject.Studio # This MUST be from Flathub and not Fedora repos to have OpenH264 support
