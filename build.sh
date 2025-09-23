efe6e0cc9910593f8f2a2755f855b635752bac7b
AJ Jordan <alex@strugee.net>
2025-09-22 22:14:46 -0400

    Install GNOME Shell extensions with BlueBuild

--- build.sh
+++ build.sh
@@ -27,13 +27,6 @@ systemctl enable seagl-reset-users.service
 
 ln -s /usr/share/applications/room-setup.desktop /etc/xdg/autostart
 
-# https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/7/html/desktop_migration_and_administration_guide/extensions-enable
-mkdir -p /usr/share/gnome-shell/extensions/{appmenu-is-back@fthx,grand-theft-focus@zalckos.github.com}
-cd /usr/share/gnome-shell/extensions/appmenu-is-back@fthx
-unzip /tmp/appmenu-is-backfthx.v3.shell-extension.zip
-cd /usr/share/gnome-shell/extensions/grand-theft-focus@zalckos.github.com
-unzip /tmp/grand-theft-focuszalckos.github.com.v7.shell-extension.zip
-chmod 644 /usr/share/gnome-shell/extensions/*/*
 cat > /etc/dconf/db/local.d/00-gnome-shell-extensions <<EOF
 [org/gnome/shell]
 enabled-extensions=['grand-theft-focus@zalckos.github.com', 'appmenu-is-back@fthx', 'places-menu@gnome-shell-extensions.gcampax.github.com', 'window-list@gnome-shell-extensions.gcampax.github.com']
