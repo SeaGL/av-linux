a42fddae45d947ee9e9df9490e2107e4e53f6030
AJ Jordan <alex@strugee.net>
2025-11-06 17:54:20 -0800

    Fix absolute path config files in birdhoused

--- apps/birdhoused/lib/config.js
+++ apps/birdhoused/lib/config.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var assert = require('assert');
-var path = require('path');
+var fs = require('fs');
 
 // We namespace to `birdhouse` so we can just reuse SeaGL/av-linux' config file
 // TODO figure out if we should like, loop waiting for config to become available in prod or something
 assert(process.argv[2], 'config file must be passed as an argument');
-var config = require(path.join('..', process.argv[2])).birdhouse;
+var config = JSON.parse(fs.readFileSync(process.argv[2])).birdhouse;
 
 module.exports = config;
 
